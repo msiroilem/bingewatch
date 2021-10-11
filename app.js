c02b6b881e70b84841261c89341e982af837193d
msiroilem <twjones86@gmail.com>
2021-10-11 12:55:47 -0400

    deleted node module named gauge and added app.js file

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const cors = require('cors')
+const logger = require('morgan')
+const bodyParser = require('body-parser')
+const path = require('path')
+const AuthRouter = require('./routes/AuthRouter')
+const ReviewRouter = require('./routes/ReviewRouter')
+const UserRouter = require('./routes/UserRouter')
+const ShowRouter = require('./routes/ShowRouter')
+const app = express()
+
+const PORT = process.env.PORT || 8080
+
+app.use(cors())
+app.use(logger('dev'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.use('/api/auth', AuthRouter)
+app.use('/api/reviews', ReviewRouter)
+app.use('/api/users', UserRouter)
+app.use('/api/shows', ShowRouter)
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'client/build')))
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(`${__dirname}/client/build/index.html`))
+  })
+}
+
+app.listen(PORT, () => console.log(`Server Running On Port: ${PORT}`))
