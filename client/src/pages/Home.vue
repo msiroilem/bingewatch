afb0b531650caeb66677e36c661864d40f33f891
msiroilem <twjones86@gmail.com>
2021-10-15 16:19:50 -0400

    trying to get TVDB API to work on Home page with getTVDBShows method

--- client/src/pages/Home.vue
+++ client/src/pages/Home.vue
@@ -1,14 +1,14 @@
 <template>
   <div>
-    <TVCard
-      v-for="show in shows"
-      :key="show.id"
-      :title="show.title"
-      :genre="show.genre"
-      :seasonNumber="show.seasonNumber"
-      :runtime="show.runtime"
-      @handleChange="handleChange"
-    />
+    <div v-for="show in shows" :key="show.id">
+      <TVCard
+        :title="show.title"
+        :genre="show.genre"
+        :seasonNumber="show.seasonNumber"
+        :runtime="show.runtime"
+        @handleChange="handleChange"
+      />
+    </div>
     <AddShow />
     <ReviewCard />
     <AddReview />
@@ -17,11 +17,12 @@
 
 <script>
 import axios from 'axios'
-import { BASE_URL } from '../globals'
+import { BASE_URL, TVDB_BASE_URL } from '../globals'
 import AddShow from '../components/AddShow.vue'
 import AddReview from '../components/AddReview.vue'
 import TVCard from '../components/TVCard.vue'
 import ReviewCard from '../components/ReviewCard.vue'
+const API_KEY = process.env.VUE_APP_TVDB_KEY
 export default {
   name: 'Home',
   components: {
@@ -31,15 +32,21 @@ export default {
     AddReview
   },
   data: () => ({
-    shows: []
+    shows: [],
+    TVDBShows: []
   }),
   mounted() {
     this.getShows()
+    this.getTVDBShows()
   },
   methods: {
     async getShows() {
       const res = await axios.get(`${BASE_URL}/shows`)
       this.shows = res.data
+    },
+    async getTVDBShows() {
+      const res = await axios.get(`${TVDB_BASE_URL}/series?key=${API_KEY}`)
+      this.TVDBShows = res.data.results
     },
     handleChange(id) {
       this.$router.push(`/shows${id}`)
