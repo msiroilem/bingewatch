39e957d71380743f4bbfcd3ae9b3d44f11c76c9d
msiroilem <twjones86@gmail.com>
2021-10-15 13:41:00 -0400

    added TVCard, ReviewCard, AddShow, AddReview components to Home page..how should I structure it?

--- client/src/pages/Home.vue
+++ client/src/pages/Home.vue
@@ -1,17 +1,26 @@
 <template>
   <div>
     <TVCard />
+    <AddShow />
+    <ReviewCard />
+    <AddReview />
   </div>
 </template>
 
 <script>
 import axios from 'axios'
 import { BASE_URL } from '../globals'
+import AddShow from '../components/AddShow.vue'
+import AddReview from '../components/AddReview.vue'
 import TVCard from '../components/TVCard.vue'
+import ReviewCard from '../components/ReviewCard.vue'
 export default {
   name: 'Home',
   components: {
-    TVCard
+    TVCard,
+    AddShow,
+    ReviewCard,
+    AddReview
   },
   data: () => ({}),
   mounted: {},
