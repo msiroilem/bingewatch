55915dc217af962669033d612001ee8b8cb5d92d
msiroilem <twjones86@gmail.com>
2021-10-15 17:19:49 -0400

    deploy heroku

--- client/src/services/Auth.js
+++ client/src/services/Auth.js
@@ -1,29 +1,17 @@
 import Client from './api'
 
 export const LoginUser = async data => {
-  try {
-    const res = await Client.post('/auth/login', data)
-    localStorage.setItem('token', res.data.token)
-    return res.data.user
-  } catch (error) {
-    throw error
-  }
+  const res = await Client.post('/auth/login', data)
+  localStorage.setItem('token', res.data.token)
+  return res.data.user
 }
 
 export const RegisterUser = async data => {
-  try {
-    const res = await Client.post('/auth/register', data)
-    return res
-  } catch (error) {
-    throw error
-  }
+  const res = await Client.post('/auth/register', data)
+  return res
 }
 
 export const CheckSession = async () => {
-  try {
-    const res = await Client.get('/auth/session')
-    return res.data
-  } catch (error) {
-    throw error
-  }
+  const res = await Client.get('/auth/session')
+  return res.data
 }
