e0d90ab12cea937f0ad92a0fc1992e217eefed1b
msiroilem <twjones86@gmail.com>
2021-10-16 16:50:33 -0400

    ReviewServices updated

--- client/src/services/ReviewServices.js
+++ client/src/services/ReviewServices.js
@@ -1,19 +1,46 @@
 import Client from '/api'
 
-TODO
-
 export const GetReview = async () => {
-  const res = await Client.get('/reviews')
+  try {
+    const res = await Client.get('/reviews')
+    return res.data
+  } catch (error) {
+    throw error
+  }
 }
 
-export const CreateReview = async () => {
-  const res = await Client.post('/reviews')
+export const GetShowReviews = async id => {
+  try {
+    const res = await Client.get(`/reviews/show/${id}`)
+    return res.data
+  } catch (error) {
+    throw error
+  }
 }
 
-export const UpdateReview = async () => {
-  const res = await Client.put('/reviews')
+export const CreateReview = async data => {
+  try {
+    const res = await Client.post('/reviews', data)
+    return res
+  } catch (error) {
+    throw error
+  }
 }
 
-export const DeleteReview = async () => {
-  const res = await Client.delete('/reviews')
+export const UpdateReview = async (id, data) => {
+  try {
+    const res = await Client.put(`/reviews/${id}`, data)
+    return res
+  } catch (error) {
+    throw error
+  }
+}
+
+export const DeleteReview = async id => {
+  try {
+    const res = await Client.delete(`/reviews/${id}`)
+    return res
+  } catch (error) {
+    throw error
+  }
 }
