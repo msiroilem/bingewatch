1637b904427f4ed5bc2caf114b3d9b1c792b8245
msiroilem <twjones86@gmail.com>
2021-10-16 12:02:45 -0400

    added MovieCard for future use

---
+++ client/src/components/MovieCard.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="movie-card" @click="selectMovie(id)">
+    <div class="info">
+      {{ title }}
+      <div class="image">
+        <img :src="`https://www.thetvdb.com${img}`" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { getImage } from '../services/TVDBServices'
+export default {
+  name: 'MovieCard',
+  methods: {
+    selectMovie(id) {
+      this.$emit('selectMovie', id)
+    },
+    getMovieImage(image) {
+      return getImage(image)
+    }
+  }
+}
+</script>
+
+<style scoped></style>
