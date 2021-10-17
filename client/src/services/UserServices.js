b7d05728dcccabb28cf2639d6f1ad7edd23e2b5f
msiroilem <twjones86@gmail.com>
2021-10-16 21:29:26 -0400

    added to UsserServies and deleted ShowServices

--- client/src/services/UserServices.js
+++ client/src/services/UserServices.js
@@ -4,3 +4,23 @@ export const CreateUser = async data => {
   const res = await Client.post('/users', data)
   return res
 }
+
+export const GetUsers = async data => {
+  const res = await Client.post('users', data)
+  return res
+}
+
+export const GetUserById = async data => {
+  const res = await Client.get('/users', data)
+  return res
+}
+
+export const UpdateUser = async data => {
+  const res = await Client.put('/users', data)
+  return res
+}
+
+export const DeleteUser = async data => {
+  const res = await Client.delete('/users', data)
+  return res
+}
